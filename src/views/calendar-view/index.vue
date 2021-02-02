b257ff5a1bb33df8ab713797be4af3e5757e6fc6
xiezhifei <xiezhifei@do1.com.cn>
2021-02-02 19:31:22 +0800

    feat: 封装日历视图组件

--- src/views/calendar-view/index.vue
+++ src/views/calendar-view/index.vue
@@ -9,9 +9,12 @@
       </div>
       <el-radio-group v-model="calendarOptions.initialView" class="view-operate" @change="changeView">
         <el-radio-button label="dayGridMonth">月</el-radio-button>
-        <el-radio-button label="timeGridWeek">周</el-radio-button>
-        <el-radio-button label="timeGridDay">日</el-radio-button>
-        <el-radio-button label="listDay">列表</el-radio-button>
+        <el-radio-button :label="isDateType ? 'dayGridWeek' : 'timeGridWeek'">周</el-radio-button>
+        <el-radio-button :label="isDateType ? 'dayGridDay' : 'timeGridDay'">日</el-radio-button>
+        <!-- <el-radio-button label="listYear">listYear列表</el-radio-button>
+        <el-radio-button label="listMonth">listMonth列表</el-radio-button>
+        <el-radio-button label="listWeek">listWeek列表</el-radio-button>
+        <el-radio-button label="listDay">listDay列表</el-radio-button> -->
       </el-radio-group>
     </div>
 
@@ -27,18 +30,19 @@
 
 <script>
 import FullCalendar from '@fullcalendar/vue'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import listPlugin from '@fullcalendar/list'
+// import dayGridPlugin from '@fullcalendar/daygrid'
+// import interactionGridPlugin from '@fullcalendar/interaction'
+// import timeGridPlugin from '@fullcalendar/timegrid'
+// import listGridPlugin from '@fullcalendar/list'
 import moment from 'moment'
 import { calendar } from './calendar.js' // 农历计算方法
 
 let calendarApi // 日历视图API
 let slotMoment = 0 // 处理双击事件
-const today = moment().format('YYYY-MM-DD')
-const displayEventTime = true // 是否显示农历
-// const myTipsDom = null
+const todayDate = moment().format('YYYY-MM-DD')
+let gridTableDom // 悬浮表格
+let tipsDom // 提示dom
+
 const dayMap = {
   0: '周日',
   1: '周一',
@@ -63,6 +67,65 @@ const monthMap = {
   12: '十二月'
 }
 
+// 格式化月/周视图
+function formatMonthAndWeek(lunarCalendar) {
+  return {
+    displayEventTime: false, // 是否显示时间
+    // 农历显示
+    dayCellContent: function(item) {
+      let itemDate = moment(item.date).format('YYYY-MM-DD')
+      const dayClass = `class="${itemDate === todayDate ? 'calendar-today' : ''} day-number"` // 添加今天样式
+      itemDate = itemDate.split('-')
+      const _dateF = calendar.solar2lunar(itemDate[0], itemDate[1], itemDate[2])
+      // 标识今天样式
+      return { html: `<p class="calendar-number-day"><span>${lunarCalendar ? _dateF.IDayCn : ''}</span><label ${dayClass}>${_dateF.cDay}号</label></p>` }
+    },
+    // 表格头部
+    dayHeaderContent(item) {
+      return { html: dayMap[item.dow] }
+    }
+  }
+}
+
+// 格式化日视图
+function formatDayGrid(lunarCalendar) {
+  return {
+    displayEventTime: false, // 是否显示时间
+    // 农历显示
+    dayCellContent: function() {
+      return { html: '' }
+    },
+    // 表格头部
+    dayHeaderContent(item) {
+      let itemDate = moment(item.date).format('YYYY-MM-DD')
+      itemDate = itemDate.split('-')
+      const _dateF = calendar.solar2lunar(itemDate[0], itemDate[1], itemDate[2])
+      return lunarCalendar ? { html: `${_dateF.ncWeek}  农历${monthMap[_dateF.lMonth]}${_dateF.IDayCn}` } : { html: _dateF.ncWeek }
+    }
+  }
+}
+
+function hideTips() {
+  tipsDom && (tipsDom.style.cssText = 'display:none;')
+}
+
+function addMouseEvent() {
+  const ableDom = document.querySelector('.fc-scrollgrid-sync-table')
+
+  if (ableDom && gridTableDom !== ableDom) {
+    const fn = (ev) => {
+      tipsDom && (tipsDom.style.cssText = `display:block;left:${ev.x + 20}px;top:${ev.y}px`)
+    }
+
+    gridTableDom && gridTableDom.removeEventListener('mousemove', fn)
+    ableDom.addEventListener('mousemove', fn)
+    ableDom.addEventListener('mouseout', () => {
+      hideTips()
+    }, false)
+    gridTableDom = ableDom
+  }
+}
+
 export default {
   name: 'CalendarView',
 
@@ -70,44 +133,67 @@ export default {
     FullCalendar
   },
 
+  props: {
+    // 开始时间
+    startDate: {
+      type: Object,
+      default: () => ({
+        type: 'DateField',
+        initialDate: ''
+      })
+    },
+    // 数据标题
+    title: {
+      type: String,
+      default: '视图标题'
+    },
+    // 默认视图
+    initialView: {
+      type: String,
+      default: 'dayGridMonth'
+    },
+    // 默认日程事件颜色
+    eventColor: {
+      type: String,
+      default: '#1890ff'
+    },
+    // 是否显示农历
+    showLunarCalendar: {
+      type: Boolean,
+      default: false
+    },
+    // 隐藏天数
+    hiddenDays: {
+      type: Array,
+      default: () => []
+    },
+    // 操作权限
+    operate: {
+      type: Object,
+      default: () => ({
+        addAble: true, // 新增
+        detailAble: true, // 查看
+        deleteable: false, // 删除
+        dropAble: true // 拖拽
+      })
+    }
+  },
+
   data() {
     return {
-      title: '视图标题',
-      shownong: false,
       viewTitle: '',
       // 日历视图配置对象
       calendarOptions: {
-        plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin], // 绑定的控件 月、日 网格插件
-        headerToolbar: { // 头部导航栏
+        plugins: [],
+        headerToolbar: {
           left: '',
           center: '',
           right: ''
         },
-        initialView: 'timeGridWeek', // 初始化视图
+        initialView: this.initialView,
+        initialDate: this.startDate && this.startDate.initialDate && moment(new Date(this.startDate.initialDate)).format('YYYY-MM-DD') || undefined, // 开始日期时间
         events: [ // 视图填充数据
           { title: 'event 1eventeventeventeventevent', date: '2021-01-23', editable: true, color: 'red', id: 222 },
-          { title: 'event 2', date: '2021-01-29 07:00' },
-          { title: 'event 2', date: '2021-01-29 07:00' },
-          { title: 'event 2', date: '2021-01-29 07:00' },
-          { title: 'event 2', date: '2021-01-29 07:00' },
-          { title: 'event 2', date: '2021-01-29 07:00' },
-          { title: 'event 2', date: '2021-01-29 07:00' },
-          { title: 'event 2', date: '2021-01-29 07:00' },
-          { title: 'event 2', date: '2021-01-29', textColor: 'red', borderColor: 'blue' },
-          { title: 'event 2', date: '2021-01-29', color: 'blue' },
-          { title: 'event 2', date: '2021-01-29', backgroundColor: 'green' },
-          // { title: 'event 2', date: '2021-01-30', display: 'background' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-29' },
-          { title: 'event 2', date: '2021-01-25' },
-          { title: 'event 2', date: '2021-01-25' },
-          { title: 'event 2', date: '2021-01-25' },
           {
             title: '标题',
             start: '2021-01-29',
@@ -165,57 +251,19 @@ export default {
             textColor: '#F9AE26' // 该事件的文字颜色
           }
         ],
-        dayMaxEventRows: true, // 显示事件数
-        // timeGridEventMinHeight: '60', // 设置事件的最小高度
+        dayMaxEventRows: true, // 开启更多事件
         locale: 'zh-cn', // 中文格式
-        views: {
-          // 月视图格式
-          dayGridMonth: {
-            displayEventTime: false, // 是否显示时间
-            // 农历显示
-            dayCellContent: this.dayCellContent
-          },
-          // 周视图格式
-          timeGridWeek: {
-            // slotMinTime: '19:00', // 周视图开始时间
-            // slotMaxTime: '20:00', // 周视图结束时间
-            displayEventTime: false, // 是否显示时间
-            // 农历显示
-            dayCellContent: this.dayCellContent,
-            // 表格头部
-            dayHeaderContent(item) {
-              return { html: dayMap[item.dow] }
-            }
-          },
-          // 日视图格式
-          timeGridDay: {
-            displayEventTime: false, // 是否显示时间
-            // 农历显示
-            dayCellContent: function() {
-              return { html: '' }
-            },
-            // 表格头部
-            dayHeaderContent(item) {
-              let itemDate = moment(item.date).format('YYYY-MM-DD')
-              itemDate = itemDate.split('-')
-              const _dateF = calendar.solar2lunar(itemDate[0], itemDate[1], itemDate[2])
-              return { html: `${_dateF.ncWeek}  农历${monthMap[_dateF.lMonth]}${_dateF.IDayCn}` }
-            }
-          }
-        },
-        // eventsSet: this.handleEvents, 获取事件日程集合
-        // weekends: false, // 是否显示周末
-        // hiddenDays: [1, 5], // 隐藏周一、周三、周五
+        // weekends: true, // 是否显示周末
+        hiddenDays: this.hiddenDays,
         aspectRatio: 1.65, // 设置日历单元格宽度与高度的比例
-        // eventColor: '#9df2b5', // 全部日历日程背景色
-        // allDaySlot: false, // 是否显示全天
+        eventColor: this.eventColor, // 全部日历日程背景色
         firstDay: 1, // 设置一周中显示的第一天是哪天，周日是0，周一是1
         allDayText: '全天',
+        // allDaySlot: false, // 是否显示全天
         defaultTimedEventDuration: '00:30', // 日程事件在时间网格中占用的高度，30分钟的高度
         slotLabelFormat: { // 左侧时间网格格式
           hour: 'numeric',
           minute: '2-digit',
-          // meridiem: 'short',
           hour12: false
         },
         eventTimeFormat: { // 在每个事件上显示的时间的格式
@@ -223,32 +271,67 @@ export default {
           minute: '2-digit',
           hour12: false
         },
-        editable: false, // 控制拖动和缩放操作
-        selectable: true, // 可以选中单元格空白处，触发 select
-        datesSet: this.datesSet,
-        select: this.dbclickRender, // 新建事件
-        eventClick: this.handleEventClick, // 点击日程事件, 已有事件删除
-        eventDrop: this.eventDrop // 拖拽成功回调
-        // eventMouseEnter: this.eventMouseEnter
-      },
-      currentEvents: []
+        editable: this.operate.dropAble, // 控制拖动和缩放操作
+        selectable: this.operate.addAble, // 可以选中单元格空白处，触发 select
+        select: this.handleEventDblClick, // 新建日程事件
+        eventClick: this.handleEventClick, // 查看日程事件
+        moreLinkClick: this.eventLimitClick,
+        eventDrop: this.eventDrop, // 拖拽成功回调
+        eventMouseEnter: hideTips, // 鼠标滑入日程事件
+        views: {
+          // 月视图格式
+          dayGridMonth: this.formatMonthorWeek(),
+          // 周视图日格式
+          dayGridWeek: this.formatMonthorWeek(),
+          // 周视图时间格式
+          timeGridWeek: this.formatMonthorWeek(),
+          // 日视图日格式
+          dayGridDay: this.formatDayGrid(),
+          // 日视图时间格式
+          timeGridDay: this.formatDayGrid()
+        }
+      }
     }
+  },
+
+  computed: {
+    isDateType() {
+      return this.startDate && (this.startDate.type === 'DateField')
+    }
+  },
+
+  created() {
+    this.loadPlugins()
   },
 
   mounted() {
     // 获取日历 API
     calendarApi = this.$refs.FullCalendar.getApi()
+    tipsDom = this.$refs.myTips
+
+    addMouseEvent() // 添加鼠标悬浮事件
     this.updateView()
-    // let tableDom = document.querySelector('.fc-scrollgrid-sync-table')
-    // if (tableDom) {
-    //   console.log(tableDom)
-    //   tableDom.addEventListener('mouseover', (event) => {
-    //     console.log('event', event)
-    //   })
-    // }
   },
 
   methods: {
+    // 加载插件
+    loadPlugins() {
+      try {
+        // 冻结对象，优化性能，防止进行响应式处理
+        // const dayGridPlugin = require('@fullcalendar/daygrid').default // 月视图
+        // const timeGridPlugin = require('@fullcalendar/timegrid').default // 周/日视图
+        // const listGridPlugin = require('@fullcalendar/list').default // 列表视图
+        // const interactionGridPlugin = require('@fullcalendar/interaction').default // eventClick 事件
+        const dayGridPlugin = Object.freeze(require('@fullcalendar/daygrid').default) // 月视图
+        const timeGridPlugin = Object.freeze(require('@fullcalendar/timegrid').default) // 周/日视图
+        const listGridPlugin = Object.freeze(require('@fullcalendar/list').default) // 列表视图
+        const interactionGridPlugin = Object.freeze(require('@fullcalendar/interaction').default) // eventClick 事件
+
+        this.calendarOptions.plugins.push(dayGridPlugin, interactionGridPlugin, timeGridPlugin, listGridPlugin)
+      } catch (error) {
+        console.log(error)
+      }
+    },
     updateView(api, value) {
       if (calendarApi) {
         try {
@@ -256,8 +339,14 @@ export default {
           if (typeof calendarApi[api] === 'function') {
             calendarApi[api](value)
           }
+          const startDate = moment(calendarApi.view.activeStar).format('YYYY-MM-DD')
+          const endDate = moment(calendarApi.view.activeEnd).format('YYYY-MM-DD')
 
-          this.viewTitle = calendarApi.currentData.viewTitle
+          // 请求接口
+          // ...
+          console.log(startDate, endDate, calendarApi.view)
+
+          this.viewTitle = calendarApi.view.title
         } catch (error) {
           console.log(error)
         }
@@ -274,39 +363,27 @@ export default {
     // 切换视图
     changeView(value) {
       this.updateView('changeView', value)
+      this.$nextTick(() => {
+        addMouseEvent()
+      })
     },
-    // 日处理
-    dayCellContent(item) {
-      let itemDate = moment(item.date).format('YYYY-MM-DD')
-      const dayClass = `class=${itemDate === today ? 'calendar-today' : ''} day-number` // 添加今天样式
-      itemDate = itemDate.split('-')
-      const _dateF = calendar.solar2lunar(itemDate[0], itemDate[1], itemDate[2])
-      // 标识今天样式
-      return { html: `<p class="calendar-month-day"><span>${displayEventTime ? _dateF.IDayCn : ''}</span><label ${dayClass}>${_dateF.cDay}号</label></p>` }
+    // 月/周视图表格标题处理
+    formatMonthorWeek() {
+      return formatMonthAndWeek(this.showLunarCalendar)
     },
-    handleDateClick: function(arg) {
-      alert('date click! ' + arg.dateStr)
+    // 格式化日视图
+    formatDayGrid() {
+      return formatDayGrid(this.showLunarCalendar)
     },
-    handleWeekendsToggle() {
-      this.calendarOptions.weekends = !this.calendarOptions.weekends // update a property
-    },
-    handleDateSelect(selectInfo) {
-      const title = prompt('Please enter a new title for your event')
-      const calendarApi = selectInfo.view.calendar
-      calendarApi.unselect() // clear date selection
-      if (title) {
-        calendarApi.addEvent({
-          id: 3333,
-          title,
-          start: selectInfo.startStr,
-          end: selectInfo.endStr,
-          allDay: selectInfo.allDay
-        })
-        // calendarApi.refetchEvents()
+    eventLimitClick(selectInfo) {
+      console.log('eventLimitClick', selectInfo)
+      const calendar = selectInfo.view.calendar
+      if (calendar) {
+        calendar.unselect() // clear date selection
+        calendar.addEvent({ id: 2233, title: 'event 2', start: selectInfo.startStr,
+          end: selectInfo.endStr })
       }
-    },
-    datesSet(args) {
-      console.log('datesSet', args)
+      return false
     },
     selectFn(selectInfo) {
       const title = prompt('请输入心间信息')
@@ -325,25 +402,24 @@ export default {
       }
     },
     handleEventClick(clickInfo) {
+      console.log(clickInfo)
+      if (!this.operate.detailAble) return
       if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
         clickInfo.event.remove()
       }
-    },
-    handleEvents(events) {
-      this.currentEvents = events
-      console.log(events)
     },
     eventDrop(data) {
       const { event, oldEvent } = data
       console.log(event, oldEvent)
     },
     // 模拟点击单元格双击事件
-    dbclickRender() {
+    handleEventDblClick(value) {
+      console.log('value', value)
       const currentMoment = new Date().getTime()
       if (currentMoment - slotMoment < 300) {
-        console.log('dbclickRender')
+        console.log('handleEventDblClick')
         // alert('双击事件')
-        // this.selectFn()
+        this.selectFn(value)
       }
 
       slotMoment = currentMoment
@@ -360,8 +436,7 @@ export default {
 </script>
 
 <style lang="scss">
-// @import '~@fullcalendar/core/main.css';
-// @import '~@fullcalendar/daygrid/main.css';
+
 .calendar-view-container {
   min-width: 1000px;
   padding: 20px;
@@ -377,49 +452,52 @@ export default {
     }
   }
   // 自定义月单元格样式
-  .calendar-month-day {
+  .calendar-number-day {
     display: flex;
     width: 100%;
     justify-content: space-between;
-    padding: 10px 4px;
-    margin: 0;
+    margin: 4px;
+    .day-number {
+      padding: 2px 8px;
+    }
     .calendar-today {
       background: #2196f3;
-      border-radius: 11px;
-      padding: 4px 8px;
+      border-radius: 10px;
       color: #fff;
-      margin-top: -4px;
     }
   }
-  // .fc-timeGridWeek-view {
-  //   .fc-scrollgrid-section-liquid {
-  //     display: none;
-  //   }
-  //   .fc-scrollgrid-sync-table,
-  //   .fc-scroller-harness,
-  //   .fc-scroller,
-  //   .fc-daygrid-body,
-  //   .fc-daygrid-day-events {
-  //     height: 100%;
-  //   }
-  // }
   .fc-timegrid-slot {
     background: #fff;
   }
   .fc-daygrid-event-harness {
     padding: 0 5px;
   }
-  .fc-daygrid-day.fc-day-today {
+  .fc .fc-highlight {
+    background: rgba(255,136,77,.05);
+  }
+  .fc .fc-daygrid-day.fc-day-today {
     background: #fff;
     .fc-daygrid-day-top {
       padding: 2px 2px 0 0;
     }
+  }
+  .fc-daygrid-body-natural .fc-daygrid-day-events {
+    margin-bottom: 10px;
+  }
+  .fc-daygrid-more-link {
+    display: inline-block;
+    padding-top: 4px;
+  }
+  .fc-more-popover .fc-popover-body {
+    max-height: 600px;
+    overflow: auto;
   }
   // 修改日历视图样式 end
   .calendar-header {
     position: relative;
     display: flex;
     justify-content: center;
+    user-select: none;
     .view-title {
       position: absolute;
       left: 0;
@@ -437,8 +515,6 @@ export default {
   .tips {
     position: fixed;
     display: none;
-    left: 5px;
-    top: 5px;
     color: #333;
     padding: 0 5px;
     z-index: 100000;
